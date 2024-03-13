18610cba77e9e9c30e7392334e2c99b340d865b8
Raphael Victor A <raphaelpictor@gmail.com>
2024-03-12 22:05:12 -0300

    Segundo exc

---
+++ dotnet_exercicio_2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Faça um Programa que converta metros para centímetros.
+
+double centimetros, metros;
+
+Console.WriteLine("Digite um valor em centimetros que deseja converter para metros: ");
+centimetros = double.Parse(Console.ReadLine());
+
+metros = centimetros /100;
+
+Console.WriteLine($"O valor convertido para metros é: {metros}m.");
