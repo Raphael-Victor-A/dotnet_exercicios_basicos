d0d80e8b35e91311a7c5800e1b40887c34a30a16
Raphael Victor A <raphaelpictor@gmail.com>
2024-03-12 22:20:35 -0300

    Quarto exc

---
+++ dotnet_exercicio_4/Program.cs
@@ -0,0 +1,18 @@
+﻿//Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+using System;
+
+double lado, area, dobroArea;
+
+Console.WriteLine("Usar a virgula caso seja numero flutuante");
+Console.ReadLine();
+
+Console.WriteLine("Me diga o valor do lado do seu quadrado");
+lado = double.Parse(Console.ReadLine());
+
+area = Math.Pow(lado, 2);
+dobroArea = area*2;
+
+
+Console.WriteLine($"A area do seu quadrado é: {area}");
+Console.WriteLine($"E o dobro da area do seu quadrado é: {dobroArea}");
