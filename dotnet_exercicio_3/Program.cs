a61ce169f195b4d07fb5a3fbf89ed9953d4edaa7
Raphael Victor A <raphaelpictor@gmail.com>
2024-03-12 22:12:21 -0300

    Terceiro exc

---
+++ dotnet_exercicio_3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+using System;
+
+double raio, area;
+const double pi = 3.14;
+
+console.WriteLine("Digite o raio do círculo: ");
+raio = double.Parse(console.ReadLine());
+
+area = pi * Math.Pow(raio, 2);
+
+console.WriteLine($"A área do círculo é: {area}");
